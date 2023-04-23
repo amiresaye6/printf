aec97619615cad61fca014659b406c0280feb9f6
Amir <amyralsyd367@gmail.com>
2023-04-23 21:00:03 +0000

    start

---
+++ _printf.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _printf - a function that produces output according to a format.
+ *
+ * @format: the format with which we will handel the input string
+ * @...:    the string we will handl
+ *
+ * Return: the number or cahracters printed without the '\0'
+ */
+
+int _printf(const char *format, ...)
+{
+	int i, temp;
+
+
+}
---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+
+#endif
